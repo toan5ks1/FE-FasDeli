3ab1b846532ecd55bfe46df1b72aace66d631844
Justinnnguyen <toannguyen@vngaynay.vn>
2022-09-06 21:17:21 +0700

    First commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import About from "./about";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <About />;
 }
 
-export default MyApp
+export default MyApp;
